930d7e896be57955756fd280327a5fdef9e90189
第二杯半价 <2807915330@qq.com>
2023-12-12 06:23:15 +0800

    feat(tabbar): 当前页面被选中时，选项高亮

--- src/components/Tabbar.vue
+++ src/components/Tabbar.vue
@@ -1,10 +1,32 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+// 当前页面的名称
+import {computed} from 'vue';
+
+const currentPageName = computed(() => {
+	const instance = getCurrentPages().at(-1)!;
+	return instance.route!.split('/').at(-1);
+});
+</script>
 
 <template>
 	<view class="tab-bar">
-		<view class="t-icon t-icon-shouye"></view>
-		<view class="t-icon t-icon-tianjia2"></view>
-		<view class="t-icon t-icon-wode"></view>
+		<navigator url="/pages/home/home" hover-class="none" class="tab-item">
+			<view
+				class="t-icon"
+				:class="
+					currentPageName === 'home' ? 't-icon-shouye-copy' : 't-icon-shouye'
+				"
+			></view>
+		</navigator>
+		<navigator hover-class="none" class="tab-item">
+			<view class="t-icon t-icon-tianjia2"></view>
+		</navigator>
+		<navigator url="/pages/mine/mine" hover-class="none" class="tab-item">
+			<view
+				class="t-icon"
+				:class="currentPageName === 'mine' ? 't-icon-wode-copy' : 't-icon-wode'"
+			></view>
+		</navigator>
 	</view>
 </template>
 
@@ -17,20 +39,30 @@
 	width: 100vw;
 	background-color: #fff;
 
-	.t-icon {
+	.tab-item {
 		flex: 1;
-		background-repeat: no-repeat;
 		height: 100%;
-		background-position: center;
-	}
+		display: flex;
+		justify-content: center;
+		align-items: center;
 
-	.t-icon-shouye,
-	.t-icon-wode {
-		background-size: 48rpx;
-	}
+		.t-icon {
+			background-repeat: no-repeat;
+			background-position: center;
+		}
 
-	.t-icon-tianjia2 {
-		background-size: 64rpx;
+		.t-icon-shouye,
+		.t-icon-wode,
+		.t-icon-shouye-copy,
+		.t-icon-wode-copy {
+			width: 48rpx;
+			height: 48rpx;
+		}
+
+		.t-icon-tianjia2 {
+			width: 64rpx;
+			height: 64rpx;
+		}
 	}
 }
 </style>
